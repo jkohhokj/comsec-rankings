84a0ea3b891fe1f0078b64f6350f7a1e075073e5
Jayden <jkohhokj@gmail.com>
2025-05-23 11:54:22 -0500

    feat: add lucide icon pack add lucide react to npm add footer to main page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 import InstitutionFrequencyChart from "./InstitutionFrequencyChart";
-
+import {Rss, Code, User} from "lucide-react";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -27,7 +27,34 @@ export default function Home() {
           conference_name="ACM CCS"
         />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
+            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href={"https://www.jkoh.dev"}
+          target="_blank"
+          rel="noopener noreferrer">
+          <User color="white" size={36} />
+            About Me
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href={"https://github.com/jkohhokj/comsec-rankings"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Code color="white" size={36} />
+          Source Code
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href={"https://blog.jkoh.dev/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Rss color="white" size={36} />
+          Blog
+        </a>
+      </footer>
     </div>
   );
 }
